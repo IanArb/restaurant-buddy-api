1c0a4f05a85e716056941d2df3f748d5e38053ad
Ian Arbuckle <ian.arbuckle18@gmail.com>
2019-08-17 12:05:48 +0100

    - Testing slashes issue for aws signature (#25) (#26)

---
+++ src/main/kotlin/com/ianarbuckle/restaurantlooker/authentication/controller/AuthenticationController.kt
@@ -0,0 +1,7 @@
+package com.ianarbuckle.restaurantlooker.authentication.controller
+
+/**
+ * @author ianarbuckle on 01/08/2019.
+ */
+class AuthenticationController {
+}
---
+++ src/main/kotlin/com/ianarbuckle/restaurantlooker/authentication/model/AuthenticationModel.kt
@@ -0,0 +1,5 @@
+package com.ianarbuckle.restaurantlooker.authentication.model
+
+/**
+ * @author ianarbuckle on 01/08/2019.
+ */
